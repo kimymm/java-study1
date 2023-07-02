c475f314611987d3567e7295823b354cc1832f90
lgv30 <lgv30@DESKTOP-9BC7B8F>
2023-07-02 22:45:56 +0900

    07/02 prac02.prob02 끝

--- practice03/src/main/java/practice03/prob02/Goods.java
+++ practice03/src/main/java/practice03/prob02/Goods.java
@@ -1,5 +1,26 @@
 package practice03.prob02;
 
 public class Goods {
-
+	private String name;
+	private int price;
+	private int number;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
 }
--- practice03/src/main/java/practice03/prob02/GoodsApp.java
+++ practice03/src/main/java/practice03/prob02/GoodsApp.java
@@ -2,7 +2,7 @@ package practice03.prob02;
 
 import java.util.Scanner; //Goods 클래스 하나 만들어야함.거기서 클래스 사용할 것이없는걸..
 
-public class GoodsApp {
+public class GoodsApp extends Goods {
 	private static final int COUNT_GOODS = 3;
 
 	public static void main(String[] args) {
@@ -10,21 +10,25 @@ public class GoodsApp {
 
 		Goods[] goods = new Goods[COUNT_GOODS]; //3개의 배열이 들어간 객체배열
 
+
 		// 상품 입력
 		for(int i=0; i < COUNT_GOODS; i++) {
-			String line = scanner.nextLine(); // next, nextline : 한줄씩 받는것
+			goods[i] = new Goods();
+			String line = scanner.nextLine(); //next, nextline : 한줄씩 받는것
+			System.out.println(line);
 			String[] info = line.split(" "); //무조건 "콜라" "1000" "10"
+			goods[i].setName(info[0]);
+			goods[i].setPrice(Integer.parseInt(info[1]));
+			goods[i].setNumber(Integer.parseInt(info[2]));
 			
-			String name = info[0];
-			int price = Integer.parseInt(info[1]);
-			int count = Integer.parseInt(info[2]); 
-			
-			
-			//setter, getter
-//			System.out.println(line);
+//			String name = info[0];
+//			int price = Integer.parseInt(info[1]);
+//			int count = Integer.parseInt(info[2]); 
 		}
 		// 상품 출력
-		System.out.println(name + "(가격:" + price + "원)이 " + count + "개 입고 되었습니다.");
+		for(int k = 0; k < COUNT_GOODS; k++) {
+			System.out.println(goods[k].getName() + "(가격:" + goods[k].getPrice() + "원)이 " + goods[k].getNumber() + "개 입고 되었습니다.");
+		}	
 		// 자원정리
 		scanner.close();
 	}
